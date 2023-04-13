0fdfdef09f3bc01912751d9d8f361eb04d5a9539
Sabit Huraira <sabitzhabit@gmail.com>
2023-04-13 11:38:28 +0700

    ignore some

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use App\Http\Controllers\Api\RutaController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('cd/user', function (Request $request) {
     return $request->user();
 });
 
