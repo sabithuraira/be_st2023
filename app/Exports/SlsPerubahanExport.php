20e5821bcd7a494e2f4cfff91b67ce5a7dc614fb
Ahmad Irfansyah <irfansyahahmad26@gmail.com>
2023-07-11 09:52:18 +0700

    Update SlsPerubahanExport.php

--- app/Exports/SlsPerubahanExport.php
+++ app/Exports/SlsPerubahanExport.php
@@ -5,10 +5,13 @@ namespace App\Exports;
 use App\Models\Sls;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class SlsPerubahanExport implements FromCollection, WithHeadings, WithMapping
+class SlsPerubahanExport implements FromCollection, WithHeadings, WithMapping, WithColumnFormatting
 {
     private $request;
 
@@ -47,7 +50,7 @@ class SlsPerubahanExport implements FromCollection, WithHeadings, WithMapping
             $sls->nama_sls,
             $sls->status_sls,
             $keterangan,
-            $sls->jml_keluarga_tani,
+            $sls->ruta_prelist,
             $sls->kode_pcl,
             $sls->kode_pml,
             $sls->kode_koseka,
@@ -67,10 +70,17 @@ class SlsPerubahanExport implements FromCollection, WithHeadings, WithMapping
             'nama_sls',
             'status_berubah_batas',
             'keterangan_sls',
-            'jml_ruta_tani_regsosek',
+            'ruta_prelist',
             'kode_pcl',
             'kode_pml',
             'kode_koseka',
+        ];
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'I' => NumberFormat::FORMAT_TEXT,
         ];
     }
 }
