4be35efba9a764fafe7bc3ff20a097d61eb2a744
Sabit Huraira <sabitzhabit@gmail.com>
2023-09-16 05:35:27 +0700

    kumkm - fix bugs on message

--- app/Http/Controllers/Api/TelegramController.php
+++ app/Http/Controllers/Api/TelegramController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Log;
 use App\Models\PesSt2023;
+use App\Models\SlsUmkm;
 use App\Http\Controllers\Controller;
 
 class TelegramController extends Controller
@@ -15,8 +16,9 @@ class TelegramController extends Controller
         //////////////FOR PRODUCTION
         $client = new \GuzzleHttp\Client();
         $update = json_decode($request->getContent());
-        Log::info('info request:', ['isi'=>$update]);
+        // Log::info('info request:', ['isi'=>$update]);
         $chatID = $update->message->chat->id;
+
         /////////////FOR TESTING
         // $message = "PES-1601052001000100-12-13-1";
         //PES-kode sls-jumlah RT tani-jumlah ART tani-selesai atau belum
@@ -25,7 +27,9 @@ class TelegramController extends Controller
             $message = $update->message->text;
 
             if(strtolower(str_replace(' ', '', $message))=='panduan'){
-                $pesan = urlencode("Kirim laporan progres PES dengan format berikut: \n
+                $pesan = urlencode("Kirim laporan progres PL-KUMKM dengan format berikut: \n
+                    <strong>KUMKM-IDSLS-Jumlah KK Sudah Cacah-NO Urut Usaha Terbesar-Jumlah Koperasi.</strong> Contoh: <pre>KUMKM-1691052001000100-12-13-1</pre> \n\n 
+                    Kirim laporan progres PES dengan format berikut: \n
                     <strong>PES-IDSLS-Jumlah Ruta Tani-Jumlah ART Tani-Apakah sudah selesai SLS ini(jika selesai isi 1 jika tidak isi 0).</strong> Contoh: <pre>PES-1691052001000100-12-13-1</pre>");
             }
             else{
@@ -35,61 +39,88 @@ class TelegramController extends Controller
                 if(count($rincian_msg)==5){
                     $msg_error = [];
                     
-                    // $cur_date = date('Y-m-d', strtotime('2022-10-18'));
-                    $cur_date = date('Y-m-d');
-
-                    if($cur_date<$awal || $cur_date>$akhir){
-                        $pesan = "Gagal!! Pelaporan hanya dapat dilakukan pada rentang 15 Oktober 2022 - 14 November 2022";  
-                    }
-                    else{
-                        $id_sls = str_replace(' ', '', $rincian_msg[1]);
-                        if(count($rincian_msg)==4){
+                    $id_sls = str_replace(' ', '', $rincian_msg[1]);
                             // $message = "PES-1601052001000100-12-13-1";
                             
-                            $kd_prov = substr($id_sls, 0,2);
-                            $kd_kab = substr($id_sls, 2,2);
-                            $kd_kec = substr($id_sls, 4,3);
-                            $kd_desa = substr($id_sls, 7,3);
-                            $kd_sls = substr($id_sls, 10,4);
-                            $kd_sub_sls = substr($id_sls, 14,2);
+                    $kd_prov = substr($id_sls, 0,2);
+                    $kd_kab = substr($id_sls, 2,2);
+                    $kd_kec = substr($id_sls, 4,3);
+                    $kd_desa = substr($id_sls, 7,3);
+                    $kd_sls = substr($id_sls, 10,4);
+                    $kd_sub_sls = substr($id_sls, 14,2);
 
-                            $data = PesST2023::where([
-                                    ['kode_prov', '=', $kd_prov],
-                                    ['kode_kab', '=', $kd_kab],
-                                    ['kode_kec', '=', $kd_kec],
-                                    ['kode_desa', '=', $kd_desa],
-                                    ['id_sls', '=', $kd_sls],
-                                    ['id_sub_sls', '=', $kd_sub_sls]
-                                ])->first();
+                    $kegiatan = str_replace(' ', '', $rincian_msg[0]);
+                    if($kegiatan=="pes"){
+                        $data = PesSt2023::where([
+                            ['kode_prov', '=', $kd_prov],
+                            ['kode_kab', '=', $kd_kab],
+                            ['kode_kec', '=', $kd_kec],
+                            ['kode_desa', '=', $kd_desa],
+                            ['id_sls', '=', $kd_sls],
+                            ['id_sub_sls', '=', $kd_sub_sls]
+                        ])->first();
                                     
-                            if($data==null){
-                                $pesan = "Identitas SLS/Non SLS ini tidak ditemukan, silahkan perbaiki.";
-                            }
-                            else{
-                            
-                                $jumlah_rt = str_replace(' ', '', $rincian_msg[2]);
-                                $jumlah_art = str_replace(' ', '', $rincian_msg[3]);
-                                $is_selesai = str_replace(' ', '', $rincian_msg[4]);
-                            
-                                if(!is_numeric($jumlah_rt)) $msg_error[] = "Isian 'Jumlah Ruta Tani' Harus Angka";
-                                if(!is_numeric($jumlah_art)) $msg_error[] = "Isian 'Jumlah ART Tani' Harus Angka";
-                                if(!is_numeric($is_selesai)) $msg_error[] = "Isian 'Informasi apakah selesai' Harus Angka";
-            
-                                if(count($msg_error)>0){
-                                    $pesan = "Error!! berikut rincian errornya : ".join(",", $msg_error);
-                                }
-                                else{
-                                    $data->jml_ruta_pes = $jumlah_rt;
-                                    $data->jml_art_pes = $jumlah_art;
-                                    $data->status_selesai = $is_selesai;
-                                    $data->save();
-                                    $pesan = "Sukses!! Laporan berhasil disimpan.";  
-                                }
-                            }
+                        if($data==null){
+                            $pesan = "Identitas SLS/Non SLS ini tidak ditemukan, silahkan perbaiki.";
                         }
                         else{
-                            "Format pesan anda salah. Balas pesan ini dengan pesan 'panduan' untuk bantuan format yang benar";  
+                            $jumlah_rt = str_replace(' ', '', $rincian_msg[2]);
+                            $jumlah_art = str_replace(' ', '', $rincian_msg[3]);
+                            $is_selesai = str_replace(' ', '', $rincian_msg[4]);
+                        
+                            if(!is_numeric($jumlah_rt)) $msg_error[] = "Isian 'Jumlah Ruta Tani' Harus Angka";
+                            if(!is_numeric($jumlah_art)) $msg_error[] = "Isian 'Jumlah ART Tani' Harus Angka";
+                            if(!is_numeric($is_selesai)) $msg_error[] = "Isian 'Informasi apakah selesai' Harus Angka";
+
+                            if(count($msg_error)>0){
+                                $pesan = "Error!! berikut rincian errornya : ".join(",", $msg_error);
+                            }
+                            else{
+                                $data->jml_ruta_pes = $jumlah_rt;
+                                $data->jml_art_pes = $jumlah_art;
+                                $data->status_selesai = $is_selesai;
+                                $data->save();
+                                $pesan = "Sukses!! Laporan berhasil disimpan.";  
+                            }
                         }
+                    }
+                    else if($kegiatan=="kumkm"){
+                        $data = SlsUmkm::where([
+                            ['kode_prov', '=', $kd_prov],
+                            ['kode_kab', '=', $kd_kab],
+                            ['kode_kec', '=', $kd_kec],
+                            ['kode_desa', '=', $kd_desa],
+                            ['id_sls', '=', $kd_sls],
+                            ['id_sub_sls', '=', $kd_sub_sls]
+                        ])->first();
+                                    
+                        if($data==null){
+                            $pesan = "Identitas SLS/Non SLS ini tidak ditemukan, silahkan perbaiki.";
+                        }
+                        else{
+                            $jumlah_kk_dicacah = str_replace(' ', '', $rincian_msg[2]);
+                            $no_urut_usaha_terbesar = str_replace(' ', '', $rincian_msg[3]);
+                            $jumlah_koperasi = str_replace(' ', '', $rincian_msg[4]);
+                        
+                            if(!is_numeric($jumlah_kk_dicacah)) $msg_error[] = "Isian 'Jumlah KK Dicacah' Harus Angka";
+                            if(!is_numeric($no_urut_usaha_terbesar)) $msg_error[] = "Isian 'No Urut Usaha Terbesar' Harus Angka";
+                            if(!is_numeric($jumlah_koperasi)) $msg_error[] = "Isian 'Jumlah Koperasi' Harus Angka";
+
+                            if(count($msg_error)>0){
+                                $pesan = "Error!! berikut rincian errornya : ".join(",", $msg_error);
+                            }
+                            else{
+                                // 'jml_kk', 'no_urut_usaha_terbesar', 'jml_koperasi'
+                                $data->jml_kk = $jumlah_kk_dicacah;
+                                $data->no_urut_usaha_terbesar = $no_urut_usaha_terbesar;
+                                $data->jml_koperasi = $jumlah_koperasi;
+                                $data->save();
+                                $pesan = "Sukses!! Laporan berhasil disimpan.";  
+                            }
+                        }
+                    }
+                    else{
+                        $pesan = "Error!! Format pesan salah, silahkan ulangi."; 
                     }
                 }
                 else{
@@ -101,7 +132,7 @@ class TelegramController extends Controller
             $pesan = "Format pesan anda salah. Balas pesan ini dengan pesan 'panduan' untuk bantuan format yang benar";  
         }
 
-        $API_message = "https://api.telegram.org/bot".env('TELEGRAM_TOKEN_REGSOSEK')."/sendmessage?chat_id=".$chatID."&text=".$pesan."&parse_mode=HTML";        
+        $API_message = "https://api.telegram.org/bot".env('TOKEN_SIMANTAP')."/sendmessage?chat_id=".$chatID."&text=".$pesan."&parse_mode=HTML";        
         $res = $client->get($API_message);
 
         return 1;
